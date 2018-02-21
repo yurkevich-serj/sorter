e2360b8fcbb9d878137c785c74a186d9e2c460af
yurkevich-serj <yurkevichserj@gmail.com>
2018-02-21 18:11:10 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
+'use strict'
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+     this.compare = function (a,b) { return a - b };
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    let array1 = [];
+    indices.sort(function(a,b) {return a-b});
+    for(let i=0; i<indices.length; i++) {
+      array1.push(this.array[indices[i]]);
+    }
+    array1.sort(this.compare);
+     
+    
+    for(let i=0; i<indices.length; i++) {
+      this.array[indices[i]]=array1[i];
+      
+    }
+    return this.array;
+    
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compare=  compareFunction;
   }
 }
 
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+'use strict'
 const assert = require('assert');
 Object.freeze(assert);
 const Sorter = require('./src/index.js');
